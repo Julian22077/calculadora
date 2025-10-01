213e0494c7610b97442164df4bd30e0522c081e9
Julian <camilolozano0122@gmail.com>
2025-09-30 19:40:28 -0500

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Contador from './Calculadora';
+import Calculadora from './Calculadora';
 function App() {
   return (
     <div className="App">
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <Contador/>
+        <Calculadora/>
       </header>
       
     </div>
--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,15 +1,74 @@
 import { useState } from "react";
-const Contador=()=>{
-    const[contador, setContador]=useState(0);
-    function Aumentar(){
-        setContador(contador+1);
+
+const Calculadora = () => {
+  const [pantalla, setPantalla] = useState("0");
+  const [guardado, setGuardado] = useState(null);
+  const [operacion, setOperacion] = useState(null);
+
+  function numero(n) {
+    if (pantalla === "0") {
+      setPantalla(String(n));
+    } else {
+      setPantalla(pantalla + String(n));
     }
-    return (
-        <div>
-        <label>Contador:{contador}</label>
-        <br/>
-        <button onClick={Aumentar}>Aumentar</button>
-        </div>
-    );
+  }
+
+  function operador(op) {
+    setGuardado(Number(pantalla));
+    setOperacion(op);
+    setPantalla("0");
+  }
+
+  function igual() {
+    if (!guardado || !operacion) return;
+
+    const n2 = Number(pantalla);
+    let res;
+
+    if (operacion === "+") res = guardado + n2;
+    if (operacion === "-") res = guardado - n2;
+    if (operacion === "*") res = guardado * n2;
+    if (operacion === "/") res = guardado / n2;
+
+    setPantalla(String(res));
+    setGuardado(null);
+    setOperacion(null);
+  }
+
+  function reset() {
+    setPantalla("0");
+    setGuardado(null);
+    setOperacion(null);
+  }
+
+  return (
+    <div className="Contador">
+      <label>Pantalla: {pantalla}</label>
+      <br />
+      <button onClick={reset}>Reset</button>
+      <br />
+      <button onClick={() => numero(1)}>1</button>
+      <button onClick={() => numero(2)}>2</button>
+      <button onClick={() => numero(3)}>3</button>
+      <br />
+      <button onClick={() => numero(4)}>4</button>
+      <button onClick={() => numero(5)}>5</button>
+      <button onClick={() => numero(6)}>6</button>
+      <br />
+      <button onClick={() => numero(7)}>7</button>
+      <button onClick={() => numero(8)}>8</button>
+      <button onClick={() => numero(9)}>9</button>
+      <br />
+      <button onClick={() => numero(0)}>0</button>
+      <br />
+      <button onClick={() => operador("+")}>+</button>
+      <button onClick={() => operador("-")}>-</button>
+      <button onClick={() => operador("/")}>/</button>
+      <button onClick={() => operador("*")}>*</button>
+      <br />
+      <button onClick={igual}>=</button>
+    </div>
+  );
 };
-export default Contador;
+
+export default Calculadora;
